7a3ad43beee54d19962c4ee5e250da4665e79902
JeongEul Kwon <wjddmf0321@naver.com>
2023-12-10 21:18:02 +0900

    modified

--- preprocessing/generation.py
+++ preprocessing/generation.py
@@ -106,7 +106,7 @@ def generate_labs(data):
     return final
 
 def generate_total_procedure(procedure, data): # micro event + procedure
-    microlabs=pd.read_csv(local+ "/features/premicrolabs_microlabs(selected).csv.gz", compression='gzip', header=0, index_col=None)
+    microlabs=pd.read_csv(local+ "/features/preproc_microlabs(selected).csv.gz", compression='gzip', header=0, index_col=None)
     microlabs=microlabs[microlabs['patientunitstayid'].isin(data['patientunitstayid'])]
     
     microlabs['start_time'] = np.rint(microlabs['lab_time_from_admit']/60)
@@ -206,3 +206,326 @@ def generate_vent(data):
     del vent['los(hour)']
     
     return vent
+
+
+def tabularization(feat_med, feat_ing, feat_out, feat_chart, feat_lab, feat_vent, feat_proc,
+                   final_meds, final_ing, final_proc, final_out, final_chart, final_labs, final_vent, valid_stay_ids, data):
+    
+    print("# Unique gender: ", data.gender.nunique())
+    print("# Unique ethnicity: ", data.ethnicity.nunique())
+    print("# Unique insurance: ", data.insurance.nunique())
+    print()
+    print("=====================")
+    print()
+    print('Number of patient: ', len(data.uniquepid.unique()))
+    print('Number of stay: ', len(data.patientunitstayid.unique()))
+    print('Expected value of observation: ', data['los(hour)'].sum())
+    print()
+    print("=====================")
+    print()
+    
+    
+    for hid in tqdm(valid_stay_ids, desc = 'Tabularize EHR for total stay 16,120'):
+        grp=data[data['patientunitstayid']==hid]
+        los = int(grp['los(hour)'].values)
+        height = grp['admissionheight'].values
+        weight = grp['admissionweight'].values 
+        if not os.path.exists(local+"/csv/"+str(hid)):
+            os.makedirs(local+"/csv/"+str(hid))
+        
+        dyn_csv=pd.DataFrame()
+        
+        ###MEDS
+        if(feat_med):
+            feat=final_meds['celllabel'].unique()
+            df2=final_meds[final_meds['patientunitstayid']==hid]
+            if df2.shape[0]==0:
+                amount=pd.DataFrame(np.zeros([los,len(feat)]),columns=feat)
+                amount=amount.fillna(0)
+                amount.columns=pd.MultiIndex.from_product([["MEDS"], amount.columns])
+            else:
+                amount=df2.pivot_table(index='start_time',columns='celllabel',values='cellvaluenumeric')
+                df2=df2.pivot_table(index='start_time',columns='celllabel',values='stop_time') #value는 큰 의미 없음
+
+                add_indices = pd.Index(range(los)).difference(df2.index) # 처방 된 시간과 los 비교 후 처방이 이루어지지 않은 시간 포인트만큼 시간 인덱스 생성
+                add_df = pd.DataFrame(index=add_indices, columns=df2.columns).fillna(np.nan) 
+                df2=pd.concat([df2, add_df])
+                df2=df2.sort_index()
+                df2=df2.ffill()
+                df2=df2.fillna(0)
+
+                amount=pd.concat([amount, add_df])
+                amount=amount.sort_index()
+                amount=amount.ffill()
+                amount=amount.fillna(0)
+    
+                df2.iloc[:,0:]=df2.iloc[:,0:].sub(df2.index,0) #end time - start time
+                df2[df2>0]=1 # 약물 처방 시간 만큼 1
+                df2[df2<0]=0 #약을 처방 받지 않은 경우에는 0으로 채웠었기 때문에 sub 연산시 음 값을 가지게 되어 0으로 변환됨
+        
+                amount.iloc[:,0:]=df2.iloc[:,0:]*amount.iloc[:,0:] # 실제 처방 된 경우의 값만 살아 남음
+                feat_df=pd.DataFrame(columns=list(set(feat)-set(amount.columns)))
+                amount=pd.concat([amount,feat_df],axis=1)
+
+
+                amount=amount[feat]
+                amount=amount.fillna(0)
+                amount.columns=pd.MultiIndex.from_product([["MEDS"], amount.columns])
+        
+                
+            if(dyn_csv.empty):
+                dyn_csv=amount
+            else:
+                dyn_csv=pd.concat([dyn_csv,amount],axis=1)
+            
+        
+        ###INGS
+        if(feat_ing):
+            feat=final_ing['drugname'].unique()
+            df2=final_ing[final_ing['patientunitstayid']==hid]
+            if df2.shape[0]==0:
+                amount=pd.DataFrame(np.zeros([los,len(feat)]),columns=feat)
+                amount=amount.fillna(0)
+                amount.columns=pd.MultiIndex.from_product([["INGS"], amount.columns])
+            else:
+                amount=df2.pivot_table(index='start_time',columns='drugname',values='amount')
+                df2=df2.pivot_table(index='start_time',columns='drugname',values='stop_time')
+                add_indices = pd.Index(range(los)).difference(df2.index)
+                add_df = pd.DataFrame(index=add_indices, columns=df2.columns).fillna(np.nan)
+                df2=pd.concat([df2, add_df])
+                df2=df2.sort_index()
+                df2=df2.ffill()
+                df2=df2.fillna(0)
+
+                amount=pd.concat([amount, add_df])
+                amount=amount.sort_index()
+                amount=amount.ffill()
+                amount=amount.fillna(0)
+                
+                df2.iloc[:,0:]=df2.iloc[:,0:].sub(df2.index,0)
+                df2[df2>0]=1
+                df2[df2<0]=0
+
+                amount.iloc[:,0:]=df2.iloc[:,0:]*amount.iloc[:,0:]
+                feat_df=pd.DataFrame(columns=list(set(feat)-set(amount.columns)))
+                amount=pd.concat([amount,feat_df],axis=1)
+
+                amount=amount[feat]
+                amount=amount.fillna(0)
+                
+                amount.columns=pd.MultiIndex.from_product([["INGS"], amount.columns])
+                
+            if(dyn_csv.empty):
+                dyn_csv=amount
+            else:
+                dyn_csv=pd.concat([dyn_csv,amount],axis=1)
+            
+            
+        ###PROCS
+        if(feat_proc):
+            feat = final_proc['treatmentstring'].unique()
+            df2 = final_proc[final_proc['patientunitstayid']==hid]
+            
+            if df2.shape[0]==0:
+                df2=pd.DataFrame(np.zeros([los,len(feat)]),columns=feat)
+                df2=df2.fillna(0)
+                df2.columns=pd.MultiIndex.from_product([["PROC"], df2.columns])
+            else:
+                df2['val']=1
+                #print(df2)
+                df2=df2.pivot_table(index='start_time',columns='treatmentstring',values='val')
+                #print(df2.shape)
+                add_indices = pd.Index(range(los)).difference(df2.index)
+                add_df = pd.DataFrame(index=add_indices, columns=df2.columns).fillna(np.nan)
+                df2=pd.concat([df2, add_df])
+                df2=df2.sort_index()
+                df2=df2.fillna(0)
+                df2[df2>0]=1
+
+                feat_df=pd.DataFrame(columns=list(set(feat)-set(df2.columns)))
+                df2=pd.concat([df2,feat_df],axis=1)
+
+                df2=df2[feat]
+                df2=df2.fillna(0)
+                df2.columns=pd.MultiIndex.from_product([["PROC"], df2.columns])
+            
+            if(dyn_csv.empty):
+                dyn_csv=df2
+            else:
+                dyn_csv=pd.concat([dyn_csv,df2],axis=1)
+            
+            
+        ###OUT
+        if(feat_out):
+            feat=final_out['celllabel'].unique()
+            df2=final_out[final_out['patientunitstayid']==hid]
+        
+            if df2.shape[0]==0:
+                val=pd.DataFrame(np.zeros([los,len(feat)]),columns=feat)
+                val=val.fillna(0)
+                val.columns=pd.MultiIndex.from_product([["OUT"], val.columns])
+            else:
+                val=df2.pivot_table(index='start_time',columns='celllabel', values='cellvaluenumeric')
+                df2['val']=1
+                df2=df2.pivot_table(index='start_time',columns='celllabel',values='val')
+
+                add_indices = pd.Index(range(los)).difference(df2.index)
+                add_df = pd.DataFrame(index=add_indices, columns=df2.columns).fillna(np.nan)
+                df2=pd.concat([df2, add_df])
+                df2=df2.sort_index()
+                df2=df2.fillna(0)
+
+                val=pd.concat([val, add_df])
+                val=val.sort_index()
+                val=val.fillna(0)
+                
+                df2[df2>0]=1
+                df2[df2<0]=0
+
+                feat_df=pd.DataFrame(columns=list(set(feat)-set(val.columns)))
+                val=pd.concat([val,feat_df],axis=1)
+
+                val=val[feat]
+                val=val.fillna(0)
+                val.columns=pd.MultiIndex.from_product([["OUT"], val.columns])
+            
+            if(dyn_csv.empty):
+                dyn_csv=val
+            else:
+                dyn_csv=pd.concat([dyn_csv,val],axis=1)
+                
+            
+        ###CHART
+        if(feat_chart):
+            feat=final_chart['nursingchartcelltypevallabel'].unique()
+            df2=final_chart[final_chart['patientunitstayid']==hid]
+            if df2.shape[0]==0:
+                val=pd.DataFrame(np.zeros([los,len(feat)]),columns=feat)
+                val=val.fillna(0)
+                val.columns=pd.MultiIndex.from_product([["CHART"], val.columns])
+            else:
+                val=df2.pivot_table(index='start_time',columns='nursingchartcelltypevallabel',values='nursingchartvalue')
+                df2['val']=1
+                df2=df2.pivot_table(index='start_time',columns='nursingchartcelltypevallabel',values='val')
+                #print(df2.shape)
+                add_indices = pd.Index(range(los)).difference(df2.index)
+                add_df = pd.DataFrame(index=add_indices, columns=df2.columns).fillna(np.nan)
+                df2=pd.concat([df2, add_df])
+                df2=df2.sort_index()
+                df2=df2.fillna(0)
+
+                val=pd.concat([val, add_df])
+                val=val.sort_index()
+                
+                ## ECMO
+                if 'ECMO' in val.columns:
+                    val['ECMO'] = val['ECMO'].notna().astype(int)
+                
+                ## MAP
+                if 'MAP' in val.columns:
+                    map_column = val['MAP'].copy()
+                    val = val.drop(columns=['MAP']).ffill()
+                    map_column = map_column.fillna((val['ABPs'] + 2*val['ABPd'])/3)
+                    val['MAP'] = map_column
+                    
+                else:
+                    val = val.ffill()
+                    val['MAP'] = (val['ABPs'] + 2*val['ABPd'])/3
+                
+        
+                df2[df2>0]=1
+                df2[df2<0]=0
+
+                feat_df=pd.DataFrame(columns=list(set(feat)-set(val.columns)))
+                val=pd.concat([val,feat_df],axis=1)
+
+                val=val[feat]
+                val.columns=pd.MultiIndex.from_product([["CHART"], val.columns])
+            
+            if(dyn_csv.empty):
+                dyn_csv=val
+            else:
+                dyn_csv=pd.concat([dyn_csv,val],axis=1)
+        
+        ###LABS
+        if(feat_lab):
+            feat=final_labs['labname'].unique()
+            df2=final_labs[final_labs['patientunitstayid']==hid]
+            if df2.shape[0]==0:
+                val=pd.DataFrame(np.zeros([los,len(feat)]),columns=feat)
+                val=val.fillna(0)
+                val.columns=pd.MultiIndex.from_product([["LAB"], val.columns])
+            else:
+                val=df2.pivot_table(index='start_time',columns='labname',values='labresult')
+                df2['val']=1
+                df2=df2.pivot_table(index='start_time',columns='labname',values='val')
+                add_indices = pd.Index(range(los)).difference(df2.index)
+                add_df = pd.DataFrame(index=add_indices, columns=df2.columns).fillna(np.nan)
+                df2=pd.concat([df2, add_df])
+                df2=df2.sort_index()
+                df2=df2.fillna(0)
+
+                val=pd.concat([val, add_df])
+                val=val.sort_index()
+                
+                val=val.ffill()
+
+                df2[df2>0]=1
+                df2[df2<0]=0
+                
+                feat_df=pd.DataFrame(columns=list(set(feat)-set(val.columns)))
+                val=pd.concat([val,feat_df],axis=1)
+
+                val=val[feat]
+                val.columns=pd.MultiIndex.from_product([["LAB"], val.columns])
+            
+            if(dyn_csv.empty):
+                dyn_csv=val
+            else:
+                dyn_csv=pd.concat([dyn_csv,val],axis=1)
+                
+                
+        
+        if(feat_vent):
+            feat=final_vent['label'].unique()
+            df2=final_vent[final_vent['patientunitstayid']==hid]
+            if df2.shape[0]==0:
+                amount=pd.DataFrame(np.zeros([los,len(feat)]),columns=feat)
+                amount=amount.fillna(0)
+                amount.columns=pd.MultiIndex.from_product([["VENT"], amount.columns])
+            else:
+                start_time = int(df2['start_time'].iloc[0])
+                stop_time = int(df2['stop_time'].iloc[0])
+                time_range = range(start_time, stop_time)
+
+                amount = pd.DataFrame(index=time_range)
+                amount['Ventilator'] = 1
+
+                add_indices = pd.Index(range(los)).difference(amount.index)
+                add_df = pd.DataFrame(index=add_indices, columns=amount.columns).fillna(np.nan)
+
+                amount=pd.concat([amount, add_df])
+                amount=amount.sort_index()
+                amount=amount.fillna(0)
+                
+                feat_df=pd.DataFrame(columns=list(set(feat)-set(amount.columns)))
+                amount=pd.concat([amount,feat_df],axis=1)
+
+                amount=amount[feat]
+                amount=amount.fillna(0)
+                
+                amount.columns=pd.MultiIndex.from_product([["VENT"], amount.columns])
+                
+            if(dyn_csv.empty):
+                dyn_csv=amount
+            else:
+                dyn_csv=pd.concat([dyn_csv,amount],axis=1)
+        
+        #[ ====== Save temporal data to csv ====== ]
+        
+        dyn_csv['height'] = height
+        dyn_csv['weight'] = weight
+      
+        dyn_csv.to_csv(local+'/csv/'+str(hid)+'/dynamic_proc.csv',index=False)
+        
+    print("[ SUCCESSFULLY SAVED TOTAL UNIT STAY DATA ]")
